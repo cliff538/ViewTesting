7c48664589152d4802cfa2da8dd1d7e44018b38d
cliff538 <cliff538@yahoo.com>
2017-11-13 16:20:12 -0700

    Comments added.

--- ViewTesting/ViewController.swift
+++ ViewTesting/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		// no auto layout feature. Square will be fixed and vary in position on each screen size.
+		let square = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+		
+		square.backgroundColor = UIColor.red
+		
+		self.view.addSubview(square)
+		
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		
+		let square2 = UIView(frame: CGRect(x: 50, y: 250, width: 200, height: 200))
+		
+		square2.backgroundColor = UIColor.orange
+		
+		self.view.addSubview(square2)
+		
 	}
 
 	override func didReceiveMemoryWarning() {
